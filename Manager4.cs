47a73c5a5291f6dbcc7778cf9ffc3b0eb396fab7
Kirill <skvorkink@gmail.com>
2023-05-19 10:22:36 +0300

    load

---
+++ Manager4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace _215_skvor
+{
+    public class Manager4
+    {
+        public static Frame MainFrame;
+    }
+}
---
+++ Task2Page.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _215_skvor
+{
+    /// <summary>
+    /// Логика взаимодействия для Task2Page.xaml
+    /// </summary>
+    public partial class Task2Page : Page
+    {
+        public Task2Page()
+        {
+            InitializeComponent();
+        }
+        private void BtnOKClick(object sender, RoutedEventArgs e)
+        {
+            //Задание 1
+
+            //double a = Convert.ToDouble(TbNumberA.Text);
+            //double b = Convert.ToDouble(TbNumberB.Text);
+            //double sum = a + b;
+            //double diff = a - b;
+            //double prod = a * b;
+            //double quot = a * a / (b * b);
+
+            //string result = $"Сумма: {sum:N2}\nРазность: {diff:N2}\nПроизведение: {prod:N2}\nЧастное квадратов: {quot:N2}";
+
+            //TextBlockAnswer.Text = $"Ответ:\n{a:N2} + {b:N2} = {sum:N2}\n{a:N2} - {b:N2} = {diff:N2}\n{a:N2} * {b:N2} = {prod:N2}\n{a:N2}^2 / {b:N2}^2 = {quot:N2}\n\n{result}";
+
+            //Задание 2
+
+            int A = Convert.ToInt32(TbNumberA.Text);
+            int B = Convert.ToInt32(TbNumberB.Text);
+
+            int temp = A;
+            A = B;
+            B = temp;
+
+            TextBlockAnswer.Text = $"Новые значения:\nA = {A}\nB = {B}";
+
+            //Задание 3
+
+            //double TF = Convert.ToDouble(TbNumberA.Text);
+            //double TC = (TF - 32) * 5 / 9;
+
+            //TextBlockAnswer.Text = $"Температура по Цельсию: {TC:N2}";
+
+            //Задание 4
+
+            //double N = 1.5;
+            //double z = N + 1 / Math.Tan(N + 1) / Math.Sin(N + 1);
+            //double c = Math.Abs(N + Math.Pow(z, 5) / (3 + z));
+
+            //TextBlockAnswer.Text = $"c = {c:N2}";
+
+        }
+    }
+}
